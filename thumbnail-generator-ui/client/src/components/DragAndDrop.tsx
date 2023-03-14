fed3b485e573411ed39abbe5734beb801d8ef04e
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-14 17:13:54 -0300

    stado redux

--- thumbnail-generator-ui/client/src/components/DragAndDrop.tsx
+++ thumbnail-generator-ui/client/src/components/DragAndDrop.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ImageUploading from 'react-images-uploading'
+import { useSelector } from 'react-redux'
 
 import { useUploadImage } from '../hooks'
 
 import { ImageSelected, BoxDragAndDrop, Message } from './'
 
 export const DragAndDrop = () => {
-  const { urlImage, handleChange, images, ...rest } = useUploadImage()
+  const imagen = useSelector((state: any) => state.yourReducer)
+
+  const { handleChange, images, ...rest } = useUploadImage()
 
   return (
     <>
-      <Message urlImages={urlImage} />
+      <Message image={images} urlImages={imagen} />
       <ImageUploading maxNumber={1} multiple={false} value={images} onChange={handleChange}>
         {({ imageList, onImageUpload, dragProps, isDragging, onImageRemove, onImageUpdate }) => (
           <>
