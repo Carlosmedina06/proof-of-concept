5e22a21993a5f473b88f922f31748171936ac38a
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-14 03:53:02 -0300

    componente titulo provisorio

---
+++ thumbnail-generator-ui/client/src/components/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <>
+      <h1 className="title">
+        <span>Upload image</span>
+        <br />
+        <span> with</span> <br />
+        <span>React & Cloudinary</span>
+      </h1>
+    </>
+  )
+}
