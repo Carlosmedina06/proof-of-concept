16246f68d76a1d4b503244f1ec2baaf8d9facff2
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-14 03:52:26 -0300

    customhook uploadimage

---
+++ thumbnail-generator-ui/client/src/hooks/useUploadImage.ts
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { ImageListType } from 'react-images-uploading'
+
+import { uploadImage } from '../redux/actions/actions'
+import { fileUpload } from '../utils'
+
+interface UploadImageHookReturnType {
+  loading: boolean
+  onUpload: () => Promise<void>
+  handleChange: (imageList: ImageListType) => void
+  urlImage: string
+  images: ImageListType
+}
+
+export const useUploadImage = (): UploadImageHookReturnType => {
+  const [images, setImages] = useState<ImageListType>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [urlImage, setUrlImage] = useState<string>('')
+  const dispatch = useDispatch()
+
+  const handleChange = (imageList: ImageListType): void => setImages(imageList)
+
+  const onUpload = async (): Promise<void> => {
+    setLoading(true)
+
+    const url = await fileUpload(images[0].file!)
+
+    setLoading(false)
+
+    if (url) {
+      dispatch(uploadImage(images))
+      setUrlImage(url.toString())
+    } else {
+      alert('Error, please try again later. ❌')
+    }
+
+    setImages([])
+  }
+
+  return {
+    loading,
+    onUpload,
+    handleChange,
+    urlImage,
+    images,
+  }
+}
