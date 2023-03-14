c71196acc607d9c9a2e22df30bcbda95e40b6c94
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-14 17:13:23 -0300

    devtools

--- thumbnail-generator-ui/client/src/redux/store/store.ts
+++ thumbnail-generator-ui/client/src/redux/store/store.ts
@@ -1,14 +1,19 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import yourReducer from '../reducers/reducers'
+import uploadImageEpic from '../epics/epics'
 
+const composeEnhancers = composeWithDevTools({})
 const rootReducer = combineReducers({
   yourReducer,
 })
 
 const epicMiddleware = createEpicMiddleware()
 
-const store = createStore(rootReducer, applyMiddleware(epicMiddleware))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)))
+
+epicMiddleware.run(uploadImageEpic)
 
 export default store
