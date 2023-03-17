5dca68d6e3dd16d3bd4288f4972422fb42a5a087
carlosmedina06 <slaawmedina14@gmail.com>
2023-03-17 15:54:10 -0300

    delete  class unnecessary

--- thumbnail-generator-ui/client/src/components/BoxDragAndDrop/BoxDragAndDrop.tsx
+++ thumbnail-generator-ui/client/src/components/BoxDragAndDrop/BoxDragAndDrop.tsx
@@ -4,14 +4,10 @@ export interface Props {
   isDragging: boolean
 }
 
-export const BoxDragAndDrop = ({ isDragging, onImageUpload, dragProps }: Props) => {
+export const BoxDragAndDrop = ({ onImageUpload, dragProps }: Props) => {
   return (
-    <div
-      onClick={onImageUpload}
-      {...dragProps}
-      className={`container-dnd center-flex ${isDragging ? 'isDragging' : ''}`}
-    >
-      <span className="label-dnd">Chosee an Image or Drag and Drop an Image 📤</span>
+    <div onClick={onImageUpload} {...dragProps}>
+      <span>Chosee an Image or Drag and Drop an Image 📤</span>
     </div>
   )
 }
